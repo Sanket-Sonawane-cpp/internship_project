bbe32a2e6131193ba3fb834a10ef6e58ec7cbd68
Sanket-Sonawane-cpp <sonawanesanketr@gmail.com>
2025-07-10 16:26:34 +0530

    Update exercises.js

--- models/exercises.js
+++ models/exercises.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
-    "Focus Area" : String,
-    "Minimum Count / Duration":String,
-    "Target Body Part":String,
-    "Exercise Type":String,
-    "Exercise Steps":String,
-    "Exercise Name":String,
-    "Benefit":String
+    "Focus Area" : {
+        type: String,
+    },
+    "Minimum Count / Duration": {
+        type: String,
+    },
+    "Target Body Part": {
+        type: String,
+    },
+    "Exercise Type": {
+        type: String,
+    },
+    "Exercise Steps": {
+        type: String,
+    },
+    "Exercise Name": {
+        type: String,
+    },
+    "Benefit": {
+        type: String,
+    },
 }, { collection: 'exercise_list' });
 
 module.exports = mongoose.model('exercises_list', exerciseSchema);
